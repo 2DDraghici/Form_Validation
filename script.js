fd5001ebb06de3e566e238371056ecc4dbf3a10f
2DDdraghici <draghici.david17@yahoo.com>
2022-12-11 16:20:23 +0200

    added a comment to script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ confirm_password  = document.querySelector("#confirm_password")
 btn = document.getElementById("create_account_button")
 
 //Added an avent listener to the button in order to check if the password was entered correctly twice
+//If they are incorrect, their border will change red
 btn.addEventListener('click',() => {
     console.log("check")
     if (password.value == confirm_password.value) {
