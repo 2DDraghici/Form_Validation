d44a0e6cdd53036757794fe9c0b64cdf0fad6c15
2DDraghici <70889720+2DDraghici@users.noreply.github.com>
2022-12-11 16:14:16 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+password = document.querySelector("#password")
+confirm_password  = document.querySelector("#confirm_password")
+btn = document.getElementById("create_account_button")
+
+//Added an avent listener to the button in order to check if the password was entered correctly twice
+btn.addEventListener('click',() => {
+    console.log("check")
+    if (password.value == confirm_password.value) {
+        console.log("all good!")
+        password.className = "correct_password"
+        confirm_password.className = "correct_password"
+        
+    }
+    else
+    {
+        console.log("They don't match")
+        password.className = "error"
+        confirm_password.className = "error"
+    }
+})
